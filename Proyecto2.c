b33f7ff60a8d9b8607d5a9a3e9582f336f929837
Joshua05p <per23395@uvg.edu.gt>
2025-04-28 12:19:15 -0600

    Update Proyecto2.c

--- Proyecto2.c
+++ Proyecto2.c
@@ -1,10 +1,9 @@
 /*
- * Libreria.c
+ * Proyecto2.c
  *
- * Created: 7/04/2025 08:58:39
+ * Created: 27/04/2025 19:23:01
  *  Author: perez
  */ 
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "Proyecto2.h"
@@ -15,7 +14,7 @@ void setupL(void) {
 	DDRD = 0xFF;
 	
 	//CONFIGURACION TIMER2
-	TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);	//Fast PWM OCR2A MAX y no invertido
+	TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);		//Fast PWM OCR2A MAX y no invertido
 	TCCR2B = (1 << WGM22) | (1 << CS22) | (1 << CS20);		//Prescaler 1024
 
 	//CONFIGURACION TIMER1
