01afcc7edccfeabeb9c5f7d8eefb958d8063f5c3
Joshua05p <per23395@uvg.edu.gt>
2025-05-05 11:28:02 -0600

    Update Proyecto2.c

--- Proyecto2.c
+++ Proyecto2.c
@@ -3,54 +3,92 @@
  *
  * Created: 27/04/2025 19:23:01
  *  Author: perez
- */ 
+ */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include "Proyecto2.h"
 
-void setupL(void) {
-	//SALIDA
-	DDRB |= (1 << DDB1) | (1<<DDB2);
-	DDRD = 0xFF;
-	
-	//CONFIGURACION TIMER2
-	TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);		//Fast PWM OCR2A MAX y no invertido
-	TCCR2B = (1 << WGM22) | (1 << CS22) | (1 << CS20);		//Prescaler 1024
 
-	//CONFIGURACION TIMER1
-	TCCR1A = (1 << COM1A1) | (1 << WGM11);
-	TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
-
-	ICR1 = 39999; // TOP = 20 ms (50 Hz): 8MHz /(8*50) = 20000 ticks
+void salidas(void) {
+       //SALIDAS 
+	   //SERVOS
+	   DDRB |=		(1 << DDB1) | (1 << DDB2);
+       DDRD |=		(1 << DDD5) | (1 << DDD3);
+	   //LEDS
+	   DDRB |=		(1 << DDB0) | (1 << DDB3);
+	   DDRD |=		(1 << DDD6);
+	   
+	   //Entradas
+	   DDRD &= ~(1<<DDD2);
+	   DDRD &= ~(1<<DDD4);
+	   PORTD |= (1 << PORTD2) | (1<<PORTD4);
+	   PCICR |=		(1<<PCIE2);
+	   PCMSK2 |=(1<<PCINT18) | (1<<PCINT20);
+	   sei();
+}
+void confi_timer2(){
+	   //CONFIGURACION TIMER2
+	   TCCR2A = (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);					// Fast PWM, OC0B no invertido
+	   TCCR2B = (1 << WGM22) | (1 << CS22) | (1 << CS20);						// Prescaler 1024
+	   OCR2A  = 156;
+}
+void confi_timer1(){
+       //CONFIGURACION TIMER1
+	   TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
+       TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);		// Prescaler 1024
+       ICR1 = 156;																
+}
+void confi_timer0(){
+	   //CONFIGURACION TIMER0
+	   CLKPR  = (1<<CLKPCE);
+	   CLKPR  = (1<<CLKPS0);													// 8MHz
+	   TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);					// Fast PWM, OC0B no invertido
+	   TCCR0B = (1 << WGM02) | (1 << CS02) | (1 << CS00);						// Prescaler 1024
+	   OCR0A  = 156;															// 20ms
 	
-	//CONFIGURACION TIMER0
-
-	TCCR0A = (1 << WGM01); //Modo CTC
-	TCCR0B = (1 << CS02) | (1 << CS00); //Prescaler 1024
 	
-	//Interrupcion timer0
-	TIMSK0 |= (1<<OCIE0B) | (1<<OCIE0A);
-	
-	OCR0A = 234;
 }
 void iniciar_adc(){
-	cli();
-	ADMUX |= (1<<REFS0);							// Voltaje de referencia 5V
-	ADMUX |= (1<<MUX2) | (1<<MUX1);					// Selecciona el canal 6
+       cli();
 
-	ADCSRA = (1 << ADEN)  |							// Habilitar ADC
-	(1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) |	// Prescaler 128 (125 kHz)
-	(1 << ADIE);									// Habilitar interrupción del ADC
+       ADMUX |= (1<<REFS0);													// Voltaje de referencia 5V
 
-	sei();
+       ADMUX |= (1<<MUX0) | (1<<MUX1);										// Selecciona el canal 3
+
+       ADCSRA = (1 << ADEN)  |												// Habilitar ADC
+				(1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) |				// Prescaler 128 (125 kHz)
+				(1 << ADIE);												// Habilitar interrupción del ADC
+
+       sei();
 }
+
+void iniciar_USART(unsigned int ubrr) {
+	UBRR0H = (ubrr >> 8);
+	UBRR0L = ubrr;
+
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0) ; // Habilitar transmisión
+
+	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+
 void iniciar_conversion(){
-	ADCSRA |= (1 << ADSC);
+       ADCSRA |= (1 << ADSC);
 }
-void ancho_pulso(uint16_t ancho) {
-	OCR1A = ancho;
+
+void servo1(uint16_t ancho1) {
+		OCR1A = ancho1;
 }
-void ancho_pulso2(uint16_t ancho2) {
-	OCR2B = ancho2;
+void servo2(uint16_t ancho2) {
+		OCR1B = ancho2;
 }
+void servo3(uint16_t ancho3) {
+		OCR0B = ancho3;
+}
+void servo4(uint16_t ancho4) {
+		OCR2B = ancho4;
+}
+
 
